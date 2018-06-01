3c357a7ca7ab28ef28b2e0414a6156030180c265
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-06-01 19:07:50 +0200

    test with no compression

--- facts.d/installed_patches.ps1
+++ facts.d/installed_patches.ps1
@@ -1,2 +1,2 @@
-$InstalledKBList = Get-Wmiobject -class Win32_QuickFixEngineering -namespace "root\cimv2" | select-object -Property HotFixID | ConvertTo-Json -Compress
-Write-Host "patch_mgmt_installed_updates=$InstalledKBList"
+$InstalledKBList = Get-Wmiobject -class Win32_QuickFixEngineering -namespace "root\cimv2" | select-object -Property HotFixID | ConvertTo-Json
+Write-Host "patch_mgmt_installed_updates=""$InstalledKBList"""
