d0f7d3e348637f0f7d22d2163971a1fd6077933f
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-06-01 18:56:25 +0200

    fix formatting issue

--- facts.d/offered_patches.ps1
+++ facts.d/offered_patches.ps1
@@ -1,3 +1,26 @@
+function New-SymLink ($link, $target)
+{
+    if ($PSVersionTable.PSVersion.Major -ge 5)
+    {
+        New-Item -Path "$link" -ItemType SymbolicLink -Value "$target"
+    }
+    else
+    {
+        $command = "cmd /c mklink /d"
+        invoke-expression "$command ""$link"" ""$target"""
+    }
+}
+
+$found = Get-ChildItem -Path $ENV:ProgramFiles\WindowsPowerShell\Modules\PSWindowsUpdate -Recurse -File PSWindowsUpdate.dll
+
+if (-Not $found) {
+    if (-Not (Test-Path $ENV:ProgramFiles\WindowsPowerShell\Modules\PSWindowsUpdate)) {
+        New-Item -Path $ENV:ProgramFiles\WindowsPowerShell\Modules\PSWindowsUpdate -ItemType Directory
+    }
+    New-SymLink -link $ENV:ProgramFiles\WindowsPowerShell\Modules\PSWindowsUpdate\PSWindowsUpdate.dll -target $ENV:ProgramData\PuppetLabs\puppet\cache\lib\patch_mgmt\PSWindowsUpdate.dll
+}
+
 Import-Module $ENV:ProgramData\PuppetLabs\puppet\cache\lib\patch_mgmt\PSWindowsUpdate.psd1
-$OfferedKBList = Get-WindowsUpdate | select-object -Property KB,Size,Title | ConvertTo-Json -Compress
-Write-Host "patch_mgmt_offered_updates=$OfferedKBList"
+$OfferedKBList = Get-WindowsUpdate
+$OfferedKBJson = $OfferedKBList | select-object -Property KB,Size,Title | ConvertTo-Json -Compress
+Write-Host "patch_mgmt_offered_updates=$OfferedKBJson"
