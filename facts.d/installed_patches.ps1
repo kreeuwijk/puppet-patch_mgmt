6a63374ff2a3d56a6f707944e29a0f17d7d92de6
Administrator <admin@example.com>
2018-06-01 14:33:29 +0000

    Update installed_patches.ps1

--- facts.d/installed_patches.ps1
+++ facts.d/installed_patches.ps1
@@ -1,2 +1,2 @@
 $InstalledKBList = Get-Wmiobject -class Win32_QuickFixEngineering -namespace "root\cimv2" | select-object -Property HotFixID | ConvertTo-Json -Compress
-Write-Host "patch_mgmt_installed_KBs=$InstalledKBList"
+Write-Host "patch_mgmt_installed_updates=$InstalledKBList"
