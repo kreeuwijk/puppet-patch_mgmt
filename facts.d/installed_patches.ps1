db4c9f4100dd8da5b9fc7129b6119fdef78f4739
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-06-01 15:44:48 +0200

    added line compression

--- facts.d/installed_patches.ps1
+++ facts.d/installed_patches.ps1
@@ -1,2 +1,2 @@
-$InstalledKBList = Get-Wmiobject -class Win32_QuickFixEngineering -namespace "root\cimv2" | select-object -Property HotFixID | ConvertTo-Json
+$InstalledKBList = Get-Wmiobject -class Win32_QuickFixEngineering -namespace "root\cimv2" | select-object -Property HotFixID | ConvertTo-Json -Compress
 Write-Host "patch_mgmt_installed_KBs=$InstalledKBList"
