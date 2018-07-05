70f768fd60be0211c7cc3bb5567bb6e984eafb30
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-07-05 17:50:30 +0200

    Puppet::ResourceApi::Command no longer exists

--- lib/puppet/provider/patch_win/patch_win.rb
+++ lib/puppet/provider/patch_win/patch_win.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'open3'
 require 'puppet/resource_api/simple_provider'
 
 # Implementation for the patch_win type using the Resource API.
@@ -7,16 +8,14 @@ class Puppet::Provider::PatchWin::PatchWin < Puppet::ResourceApi::SimpleProvider
   # confine kernel: 'windows'
   # confine patch_mgmt_psmodule: true
 
-  def initialize
-    powershell = if File.exist?("#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")
-                   "#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe"
-                 elsif File.exist?("#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe")
-                   "#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe"
-                 else
-                   'powershell.exe'
-                 end
-    @patch_win_cmd = Puppet::ResourceApi::Command.new powershell
-  end
+  commands :powershell =>
+    if File.exists?("#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")
+      "#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe"
+    elsif File.exists?("#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe")
+      "#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe"
+    else
+      'powershell.exe'
+    end
 
   def get(context)
     get_patches = <<~EOS
@@ -32,8 +31,8 @@ class Puppet::Provider::PatchWin::PatchWin < Puppet::ResourceApi::SimpleProvider
       }
       $PatchList | ConvertTo-JSON
     EOS
-    result = @patch_win_cmd.run(context, get_patches)
-    result
+    stdin, stdout, stderr = Open3.popen3(powershell([get_patches]))
+    stdout
   end
 
   def create(context, name, should)
