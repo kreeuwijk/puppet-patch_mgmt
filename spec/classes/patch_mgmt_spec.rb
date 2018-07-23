5483ff3aad875236c6f8275d2ee2e304997d75ff
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-07-23 15:25:20 +0200

    Added powershell_version fact

--- spec/classes/patch_mgmt_spec.rb
+++ spec/classes/patch_mgmt_spec.rb
@@ -5,7 +5,10 @@ require 'spec_helper'
 describe 'patch_mgmt' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) { os_facts }
+      let(:facts) {
+        os_facts
+        facts.merge({ :powershell_version => '4.0' })
+      }
 
       it { is_expected.to compile }
     end
