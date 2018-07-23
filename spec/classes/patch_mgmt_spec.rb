72ae5cfee6ecdf7b0f859fa4f56ca3e485b611d5
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-07-23 15:31:19 +0200

    added patch_mgmt_psmodulepath custom fact value

--- spec/classes/patch_mgmt_spec.rb
+++ spec/classes/patch_mgmt_spec.rb
@@ -5,10 +5,12 @@ require 'spec_helper'
 describe 'patch_mgmt' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) {
-        os_facts
-        facts.merge({ :powershell_version => '4.0' })
-      }
+      let(:facts) do
+        facts.merge({
+          :powershell_version      => '4.0',
+          :patch_mgmt_psmodulepath => 'C:\Program Files\WindowsPowerShell\Modules',
+        })
+      end
 
       it { is_expected.to compile }
     end
